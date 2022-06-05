bf2961e3edbcabb8b0c2ad43b7c1a6cab5302954
Alejandrofgu <agu528@inlumine.ual.es>
2022-06-05 04:26:14 +0200

    Additions and fixes.Ready

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 const video = document.getElementById('video');
+const content = document.getElementById('content');
 const mask = document.getElementById('mask');
 const toggleDetect = document.getElementById('detect');
 const toggleMask = document.getElementById('toggmask');
+const toggleLandMark = document.getElementById('toggland');
 
 detecting = false;
 masked = false;
+land = false;
 var intervalo = null;
 var intervalo2 = null;
-const labels = ['Alejandro']
-const imageUrl = 'https://i.ibb.co/fCVgG7J/rd2.png';
+const labels = ['Alejandro', 'Rosangela']
 
 Promise.all([
     faceapi.loadFaceLandmarkModel('/models'),
     faceapi.loadFaceRecognitionModel('/models'),
-    faceapi.loadTinyFaceDetectorModel('/models'),
+    // faceapi.loadTinyFaceDetectorModel('/models'),
     faceapi.loadSsdMobilenetv1Model('/models')
-
 
 ]).then(() => {
     startCamara()
-    training()
+    training();
 }
 );
 function startCamara() {
@@ -33,7 +34,8 @@ var labeledFaceDescriptors = null;
 async function training() {
     labeledFaceDescriptors = await Promise.all(
         labels.map(async label => {
-            const img = await faceapi.fetchImage(imageUrl);
+            const imgUrl = `/images/${label}.png`
+            const img = await faceapi.fetchImage(imgUrl)
             const fullFaceDescription = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
             if (!fullFaceDescription) {
                 throw new Error(`no faces detected for ${label}`)
@@ -46,41 +48,44 @@ async function training() {
 toggleMask.addEventListener('click', () => {
     toggleMaskMethod()
 });
+toggleLandMark.addEventListener('click', () => {
+    toggleLandMarksMethod()
+});
 toggleDetect.addEventListener('click', () => {
     const canvas = faceapi.createCanvasFromMedia(video);
+    content.appendChild(canvas);
     context = canvas.getContext('2d');
     toggleDetectMethod();
     if (detecting) {
-        document.body.append(canvas);
         const displaySize = { width: video.width, height: video.height };
         faceapi.matchDimensions(canvas, displaySize);
         intervalo = setInterval(async () => {
-            let detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptors();
+            let detections = await faceapi.detectAllFaces(video).withFaceLandmarks().withFaceDescriptors();
+            let fullFaceDescriptions = faceapi.resizeResults(detections, displaySize);
+           
             const maxDescriptorDistance = 0.6;
             const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, maxDescriptorDistance);
             const results = detections.map(fd => faceMatcher.findBestMatch(fd.descriptor));
+            
             results.forEach((bestMatch, i) => {
-                const box = detections[i].detection.box;
-                const text = bestMatch.toString();
-                const drawBox = new faceapi.draw.DrawBox(box, { label: text });
-                if(text.includes("Alejandro")){
-                    if (masked) {
-                        base_image = new Image();
-                        base_image.src = '/images/overlay-red-monster2.png';
-                        base_image.onload = function(){
-                            context.drawImage(base_image, drawBox.box._x, drawBox.box._y,drawBox.box.width,drawBox.box.height/1.2);
-                       }
-                    } else {
-                        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-                    }
-                }else{
-                    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+                const box = fullFaceDescriptions[i].detection.box
+                const text = bestMatch.toString()
+                const drawBox = new faceapi.draw.DrawBox(box, { label: text })
+                if (masked) {
+                    base_image = new Image();
+                    base_image.src = '/images/overlay-red-monster2.png';
+                    base_image.onload = function(){
+                        context.drawImage(base_image, drawBox.box._x, drawBox.box._y,drawBox.box.width,drawBox.box.height/1.3);
+                   }
+                }
+                if(land){
+                    faceapi.draw.drawFaceLandmarks(canvas, fullFaceDescriptions);
                 }
                 drawBox.draw(canvas)
-                intervalo2 = setTimeout(function () {
-                    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-                }, 30);
             })
+            intervalo2 = setTimeout(function () {
+                    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+            }, 50);
         }, 100);
     } else {
         canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
@@ -102,5 +107,14 @@ function toggleMaskMethod() {
         toggleMask.innerText = 'Unmask';
     } else {
         toggleMask.innerText = 'Mask';
+    }
+}
+
+function toggleLandMarksMethod() {
+    land = !land;
+    if (land) {
+        toggleLandMark.innerText = 'UnLandMarks';
+    } else {
+        toggleLandMark.innerText = 'LandMask';
     }
 }
